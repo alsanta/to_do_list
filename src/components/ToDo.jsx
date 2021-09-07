163295bcdd67b8b2d1b969a3f36a60a7941cf2f7
Alex Santana <alexandresntn@yahoo.com>
2021-09-06 22:05:48 -0500

    main

---
+++ src/components/ToDo.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+const ToDo = (props) => {
+    return (
+        <>
+        <div>
+            <form action="">
+                <input type="text" />
+                <input type="checkbox" name="" id="" />
+            </form>
+        </div>
+        </>
+    );
+}
+
+export default ToDo;
